306a111defdec5eb3c8766f58120b8522ded8f7e
Vladimir Jovin <vladimirjovin8@gmail.com>
2024-12-11 02:08:22 +0100

    We are seriously actually doing something, now this is good

---
+++ devjam-ebpf/src/main.rs
@@ -0,0 +1,105 @@
+#![no_std]
+#![no_main]
+
+use core::{mem, ptr, usize};
+
+use aya_ebpf::{
+    bindings::xdp_action::{self, XDP_PASS},
+    helpers::bpf_ktime_get_ns,
+    macros::{map, xdp},
+    maps::RingBuf,
+    programs::XdpContext,
+};
+use aya_log_ebpf::info;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::{IpProto, Ipv4Hdr},
+    udp::UdpHdr,
+};
+
+#[map]
+static DNS_RESPONSES_RING_BUFFER: RingBuf = RingBuf::with_byte_size(16_777_216u32, 0);
+
+#[xdp]
+pub fn devjam(ctx: XdpContext) -> u32 {
+    match try_devjam(ctx) {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+#[inline(always)]
+unsafe fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    let ptr = (start + offset) as *const T;
+    Ok(&*ptr)
+}
+
+fn try_devjam(ctx: XdpContext) -> Result<u32, ()> {
+    let ethhdr: *const EthHdr = unsafe { ptr_at(&ctx, 0)? };
+    match unsafe { (*ethhdr).ether_type } {
+        EtherType::Ipv4 => {}
+        _ => return Ok(xdp_action::XDP_PASS),
+    }
+
+    let ipv4hdr: *const Ipv4Hdr = unsafe { ptr_at(&ctx, EthHdr::LEN) }?;
+
+    match unsafe { (*ipv4hdr).proto } {
+        IpProto::Tcp => {
+            return Ok(XDP_PASS);
+        }
+        IpProto::Udp => {
+            let udphdr: *const UdpHdr = unsafe { ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN) }?;
+            if u16::from_be(unsafe { (*udphdr).source }) == 53 {}
+        }
+        _ => return Err(()),
+    };
+
+    const U16_SIZE: usize = mem::size_of::<u16>();
+    const U64_SIZE: usize = mem::size_of::<u64>();
+    const SIZE: usize = U16_SIZE + U64_SIZE + 1500;
+
+    match DNS_RESPONSES_RING_BUFFER.reserve::<[u8; SIZE]>(0) {
+        Some(mut event) => {
+            let len = ctx.data_end() - ctx.data();
+
+            if aya_ebpf::check_bounds_signed(len as i64, 1, 1500) == false {
+                event.discard(0);
+                return Ok(xdp_action::XDP_PASS);
+            }
+
+            unsafe {
+                ptr::write_unaligned(event.as_mut_ptr() as *mut _, len as u16);
+                let now = bpf_ktime_get_ns();
+                ptr::write_unaligned(event.as_mut_ptr().byte_add(U16_SIZE) as *mut _, now);
+
+                match aya_ebpf::helpers::gen::bpf_xdp_load_bytes(
+                    ctx.ctx,
+                    0,
+                    event.as_mut_ptr().byte_add(U16_SIZE).byte_add(U64_SIZE) as *mut _,
+                    len as u32,
+                ) {
+                    0 => event.submit(0),
+                    _ => event.discard(0),
+                }
+            }
+        }
+        None => {
+            info!(&ctx, "Cannot reserve space in ring buffer.");
+        }
+    };
+    return Ok(XDP_PASS);
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
---
+++ devjam/src/main.rs
@@ -0,0 +1,260 @@
+use chrono::{self, DateTime, Utc};
+use serde::ser::SerializeStruct;
+use std::convert::Infallible;
+use std::sync::Arc;
+use std::{ptr, slice};
+use warp::http::StatusCode;
+use warp::reply::{self, Reply};
+use warp::Filter;
+
+use anyhow::{Context as _, Ok};
+
+use aya::programs::{Xdp, XdpFlags};
+use clap::Parser;
+use dns_parser::ResourceRecord;
+use log::{debug, info, warn};
+use serde::Serialize;
+use tokio::io::unix::AsyncFd;
+use tokio::join;
+use tokio::sync::{mpsc, watch, RwLock};
+
+#[derive(Debug, Parser)]
+struct Opt {
+    #[clap(short, long, default_value = "enp5s0")]
+    iface: String,
+}
+
+type DnsResponse<'a> = (Vec<MyResourceRecord<'a>>, DateTime<Utc>);
+
+type Universe<'a> = Arc<RwLock<Vec<DnsResponse<'a>>>>;
+
+#[derive(Serialize)]
+struct DnsResponseWrapper<'a> {
+    responses: Vec<DnsResponse<'a>>,
+}
+
+#[derive(Debug)]
+struct MyResourceRecord<'a>(pub ResourceRecord<'a>);
+
+impl<'a> Clone for MyResourceRecord<'a> {
+    fn clone(&self) -> Self {
+        MyResourceRecord(ResourceRecord {
+            name: self.0.name,
+            multicast_unique: self.0.multicast_unique,
+            cls: self.0.cls,
+            ttl: self.0.ttl,
+            data: match &self.0.data {
+                dns_parser::RData::A(record) => dns_parser::RData::A(*record),
+                dns_parser::RData::AAAA(record) => dns_parser::RData::AAAA(*record),
+                dns_parser::RData::CNAME(record) => dns_parser::RData::CNAME(*record),
+                dns_parser::RData::MX(record) => dns_parser::RData::MX(*record),
+                dns_parser::RData::NS(record) => dns_parser::RData::NS(*record),
+                dns_parser::RData::PTR(record) => dns_parser::RData::PTR(*record),
+                dns_parser::RData::SOA(record) => dns_parser::RData::SOA(*record),
+                dns_parser::RData::SRV(record) => dns_parser::RData::SRV(*record),
+                dns_parser::RData::TXT(record) => dns_parser::RData::TXT(record.clone()),
+                dns_parser::RData::Unknown(data) => dns_parser::RData::Unknown(data.clone()),
+            },
+        })
+    }
+}
+
+impl Serialize for MyResourceRecord<'_> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut state = serializer.serialize_struct("ResourceRecord", 5)?;
+
+        state.serialize_field("name", &self.0.name.to_string())?;
+        state.serialize_field("multicast_unique", &self.0.multicast_unique)?;
+        // state.serialize_field("cls", &self.cls as usize)?;
+        state.serialize_field("ttl", &self.0.ttl)?;
+        match &self.0.data {
+            dns_parser::RData::A(_record) => {
+                state.serialize_field("record_type", "A")?;
+            }
+            dns_parser::RData::AAAA(_record) => {
+                state.serialize_field("record_type", "AAAA")?;
+            }
+            dns_parser::RData::CNAME(_record) => {
+                state.serialize_field("record_type", "CNAME")?;
+            }
+            dns_parser::RData::MX(_record) => {
+                state.serialize_field("record_type", "MX")?;
+            }
+            dns_parser::RData::NS(_record) => {
+                state.serialize_field("record_type", "NS")?;
+            }
+            dns_parser::RData::PTR(_record) => {
+                state.serialize_field("record_type", "PTR")?;
+            }
+            dns_parser::RData::SOA(_record) => {
+                state.serialize_field("record_type", "SOA")?;
+            }
+            dns_parser::RData::SRV(_record) => {
+                state.serialize_field("record_type", "SRV")?;
+            }
+            dns_parser::RData::TXT(_record) => {
+                state.serialize_field("record_type", "TXT")?;
+            }
+            dns_parser::RData::Unknown(_) => todo!(),
+        }
+        state.end()
+    }
+}
+
+async fn event_collector<'a>(
+    mut rx: mpsc::Receiver<DnsResponse<'a>>,
+    received_data: Arc<RwLock<Vec<DnsResponse<'a>>>>,
+) {
+    info!("Started event collector");
+    while let Some(value) = rx.recv().await {
+        // Acquire a write lock to modify the received_data
+        let mut data = received_data.write().await;
+        data.push(value);
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let opt = Opt::parse();
+
+    env_logger::init();
+
+    // Bump the memlock rlimit. This is needed for older kernels that don't use the
+    // new memcg based accounting, see https://lwn.net/Articles/837122/
+    let rlim = libc::rlimit {
+        rlim_cur: libc::RLIM_INFINITY,
+        rlim_max: libc::RLIM_INFINITY,
+    };
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+    if ret != 0 {
+        debug!("remove limit on locked memory failed, ret is: {}", ret);
+    }
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
+        env!("OUT_DIR"),
+        "/devjam"
+    )))?;
+    if let Err(e) = aya_log::EbpfLogger::init(&mut ebpf) {
+        // This can happen if you remove all log statements from your eBPF program.
+        warn!("failed to initialize eBPF logger: {}", e);
+    }
+    let Opt { iface } = opt;
+    let program: &mut Xdp = ebpf.program_mut("devjam").unwrap().try_into()?;
+    program.load()?;
+    program.attach(&iface, XdpFlags::default())
+        .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
+    let ring_dump =
+        aya::maps::RingBuf::try_from(ebpf.take_map("DNS_RESPONSES_RING_BUFFER").unwrap()).unwrap();
+
+    let (_tx, rx) = watch::channel(false);
+    let (t_event, r_event_collector): (mpsc::Sender<DnsResponse>, mpsc::Receiver<DnsResponse>) =
+        mpsc::channel(50);
+
+    let received_data = Arc::new(RwLock::new(Vec::new()));
+
+    let read_buffer = tokio::spawn(async move {
+        let mut rx = rx.clone();
+        let t_event = t_event.clone();
+        let mut async_fd = AsyncFd::new(ring_dump).unwrap();
+
+        loop {
+            tokio::select! {
+                _ = async_fd.readable_mut() => {
+                let  t_event = t_event.clone();
+
+                    let mut guard = async_fd.readable_mut().await.unwrap();
+                    let rb = guard.get_inner_mut();
+
+                    while let Some(read) = rb.next() {
+                        let ptr = read.as_ptr();
+
+                        let size = unsafe { ptr::read_unaligned::<u16>(ptr as *const u16) };
+
+                        // relevant commets below, cause there is some code
+                        let mut _timestamp = unsafe { ptr::read_unaligned::<u64>(ptr.byte_add(2) as *const u64) };
+                        // checking for timestamp diff, as the timestamp that is read from the DNS_RESPONSES_RING_BUFFER is the timestamp since boot time, so we would need some magic to actually convert this
+                        // but, my testing on a usual load indicates this is at max < 100 ms, so we are fine, for PoC for sure
+                        // thus we will be concating userspace time when this was read from ring buffer
+                        // dbg!(&timestamp);
+                        // timestamp = u64::MAX - timestamp.wrapping_sub(std::time::Duration::from(nix::time::clock_gettime(nix::time::ClockId::CLOCK_MONOTONIC).unwrap()).as_nanos() as u64);
+
+                        let data = unsafe { slice::from_raw_parts(ptr.byte_add(2).byte_add(8), size.into()) };
+                        let reading_time = chrono::offset::Utc::now();
+
+                        if let std::result::Result::Ok(response_packet) = dns_parser::Packet::parse(&data[42_usize..size as usize]) {
+                            let my_records: Vec<MyResourceRecord> = response_packet.answers.into_iter().map(MyResourceRecord).collect();
+                            t_event.send((my_records, reading_time)).await.unwrap();
+                        }
+                        else{
+                            // Now this is the funny part I do not understand, thing is, if for some reason header is extended
+                            // it does not start at start + 42, so we need to bruteforce find where the actual packet starts that
+                            // is parsable, consider exposing this as metric if this project ever expands
+                            for i in 0..size {
+                                match dns_parser::Packet::parse(&data[i as usize..size as usize]) {
+                                    Err(_) => {},
+                                 std::result::Result::Ok(response_packet) => {
+                                    let my_records: Vec<MyResourceRecord> = response_packet.answers.into_iter().map(MyResourceRecord).collect();
+                                    t_event.send((my_records, reading_time)).await.unwrap();
+                                break;
+                                },
+                            }
+                            }
+                        }
+                    }
+
+                    guard.clear_ready();
+                },
+                _ = rx.changed() => {
+                    if *rx.borrow() {
+                        break;
+                    }
+                }
+            }
+        }
+    });
+
+    let collector = {
+        let received_data = Arc::clone(&received_data);
+        tokio::spawn(async move {
+            event_collector(r_event_collector, received_data).await;
+        })
+    };
+
+    let warp_handle = {
+        let get_universe_route = warp::path("universe")
+            .and(warp::get())
+            .and(with_universe(received_data.clone()))
+            .and_then(get_universe);
+        tokio::spawn(async move {
+            warp::serve(get_universe_route)
+                .run(([127, 0, 0, 1], 3030))
+                .await;
+        })
+    };
+
+    join!(collector, read_buffer, warp_handle);
+    info!("Exiting...");
+    Ok(())
+}
+
+fn with_universe(
+    universe: Universe,
+) -> impl Filter<Extract = (Universe,), Error = Infallible> + Clone {
+    warp::any().map(move || universe.clone())
+}
+
+async fn get_universe(universe: Universe<'_>) -> Result<impl Reply, warp::Rejection> {
+    let universe = universe.read().await;
+    let response = DnsResponseWrapper {
+        responses: universe.clone(),
+    };
+
+    std::result::Result::Ok(reply::with_status(reply::json(&response), StatusCode::OK))
+}
